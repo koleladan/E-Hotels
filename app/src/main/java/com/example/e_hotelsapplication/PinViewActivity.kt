26bd08676b36f1a5572f0f25a0e6fc1b39781a1a
koleladan <isaiahodan@gmail.com>
2023-01-12 22:39:54 +0300

    logic implementation

--- app/src/main/java/com/example/e_hotelsapplication/PinViewActivity.kt
+++ app/src/main/java/com/example/e_hotelsapplication/PinViewActivity.kt
@@ -1,11 +1,17 @@
 package com.example.e_hotelsapplication
 
+import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.e_hotelsapplication.databinding.ActivityPinViewBinding
 
 class PinViewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPinViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pin_view)
+        binding = ActivityPinViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val actionBar: androidx.appcompat.app.ActionBar? =supportActionBar
+        actionBar?.hide()
     }
 }
--- app/src/main/java/com/example/e_hotelsapplication/SplashScreenActivity.kt
+++ app/src/main/java/com/example/e_hotelsapplication/SplashScreenActivity.kt
@@ -1,11 +1,26 @@
 package com.example.e_hotelsapplication
 
+import android.app.ActionBar
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.e_hotelsapplication.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val actionBar: androidx.appcompat.app.ActionBar? =supportActionBar
+        actionBar?.hide()
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this@SplashScreenActivity, PinViewActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+            finish()
+        }, 2000)
+
+
+
     }
 }
